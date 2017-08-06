9d254e37da77da8cb282494f2f914d0f30a9587d
Herb9 <pksxtm@gmail.com>
2017-08-06 21:57:17 +0900

    Initial commit

---
+++ EssentialsPro/EssentialsPro/EssentialsPro.cs
@@ -0,0 +1,33 @@
+﻿
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+/*
+    ########  #### ########    ###    
+    ##     ##  ##  ##         ## ##   
+    ##     ##  ##  ##        ##   ##  
+    ########   ##  ######   ##     ## 
+    ##         ##  ##       ######### 
+    ##         ##  ##       ##     ## 
+    ##        #### ######## ##     ## 
+    
+    PIEA, MINET Developers' Team.
+*/
+
+namespace EssentialsPro
+{
+    [Plugin(PluginName = "EssentialsPro", Description = "Preparing...", PluginVersion = "1.0", Author = "PIEA")]
+    public class EssentialsPro : Plugin
+    {
+        protected override void OnEnable()
+        {
+
+        }
+
+        public override void OnDisable()
+        {
+
+        }
+    }
+}
